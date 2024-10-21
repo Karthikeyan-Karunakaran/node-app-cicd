180acc15d4e6d1b04000c2cd93fabfcf743f3144
Karthikeyan.K <93427303+Karthikeyan21001828@users.noreply.github.com>
2024-10-21 12:21:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-res.send('Hello, DevOps World from Node.js!');
+res.send('Hello, DevOps World from Node.js! Kasthuri');
 });
 app.listen(port, () => {
 console.log(`App listening at http://localhost:${port}`);
